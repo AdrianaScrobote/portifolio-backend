de002ec4c7d58cb9b7ac6af404cc7da844cf0ac4
AdrianaScrobote <adriana_100000@gmail.com>
2020-09-27 11:35:24 -0300

    Add funcionalidade de gravar contato

--- contato.js
+++ contato.js
@@ -1,12 +1,66 @@
-const contato = function(app){
+const contato = async function(app){
     
     const db = require('./model/database')
+    
+    app.post('/contato/gravar/', async function(req, res){
 
-    app.post('/contato/gravar/', function(req, res){
+        let data = [],
+        error = [],
+        statusCodeResponse = 200,
+        response = {
+        statusCode: null,
+        body: null
+        };
 
-        let retorno = {"msg": "Os dados foram cadastrados com sucesso!"}
-        res.json(retorno);
-        res.end();
+        try {
+            await db.startConnetion()
+
+            if(await db.isConnected()){
+
+                let contato = await db.queryExec(`
+                            insert into contato
+                                (nome, 
+                                email, 
+                                assunto, 
+                                mensagem, 
+                                dt_cadastro)
+                            values 
+                                ('`+ req.body.nome +`', 
+                                '`+ req.body.email +`', 
+                                '`+ req.body.assunto +`',
+                                '`+ req.body.mensagem +`', 
+                                '`+ req.body.dt_cadastro +`')
+                            `)
+
+                if(contato.status == 'error'){
+                    statusCodeResponse = 500
+                    error.push({input: contato.body, msg: contato.message})
+                }
+                else{
+                    data.push({input: contato.body, msg: "Contato cadastrado com sucesso!"})
+                }
+            }
+            else{
+                statusCodeResponse = 500
+                error.push({input: contato.body, msg: "Não foi possível realizar a conexão com o banco de dados"})
+            }
+        } catch (e) {
+            statusCodeResponse = 404;
+            error.push(e.stack);
+        } finally {
+          
+            await db.closeConnection()
+
+            response.statusCode = statusCodeResponse;
+        
+            response.body = {
+            data: data,
+            errors: error
+            };
+
+            res.json(response);
+            res.end();
+        }
     })
 }
 
--- model/database.js
+++ model/database.js
@@ -1,17 +1,88 @@
-const seq = require('sequelize')
+const sequelize = require('sequelize')
 require('mysql2')
 
-// BD
-const banco_dados = new seq(
-    'portifolio', 
-    'root', '', {
-        host: 'localhost',
-        dialect: 'mysql'
-    }
-)
+class databaseManager{
 
-// Exports
-module.exports = {
-  seq: seq,
-  banco_dados: banco_dados
+  // Criação do singleton
+  constructor() {
+    if (!databaseManager.instance) {
+      this.models = {};
+      this.connection = false;
+      databaseManager.instance = this;
+    }
+
+    return databaseManager.instance;
+  }
+
+  async startConnetion(){
+
+    try{
+      this.connection = new sequelize(
+        'portifolio', 
+        'root', '', {
+            host: 'localhost',
+            dialect: 'mysql',
+            logging: false, // não retornar logs
+        }
+      )
+
+      await this.connection
+        .authenticate()
+        .then(async () => {
+          await console.log("Conexao realizada com sucesso");
+        })
+        .catch(err => {
+          throw new Error(err);
+        });
+    }
+    catch (err) {
+      new Error(JSON.stringify({ message: err.message }));
+    }
+  }
+
+  async isConnected() {
+    if (!this.connection) {
+      return false;
+    }
+    return true;
+  }
+
+  async queryExec(query, rtnIdInsert = false) {
+    try {
+      let response = null,
+        typeOper = null,
+        operator = query
+          .trim()
+          .split(" ")
+          .shift()
+          .toUpperCase();
+
+      if (operator == "SELECT") typeOper = sequelize.QueryTypes.SELECT;
+      else if (operator == "INSERT") typeOper = sequelize.QueryTypes.INSERT;
+      else if (operator == "UPDATE") typeOper = sequelize.QueryTypes.UPDATE;
+      else typeOper = sequelize.QueryTypes.SELECT
+
+      response = await this.connection.query(query, { type: typeOper });
+      return response;
+      
+    } catch (err) {
+      return {
+        status: "error",
+        message: err.message
+      };
+    }
+  }
+
+  // Finaliza a conexão com o banco de dados
+  async closeConnection() {
+    try {
+      await this.connection.close();
+      this.connection = null;
+      return true;
+    } catch (err) {
+      new Error(JSON.stringify({ message: err.message }));
+    }
+  }
 }
+
+module.exports = new databaseManager();
